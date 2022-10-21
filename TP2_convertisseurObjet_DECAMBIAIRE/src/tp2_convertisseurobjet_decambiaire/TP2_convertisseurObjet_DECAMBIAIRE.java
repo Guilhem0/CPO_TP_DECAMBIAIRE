419fc08453c9b0664cde00ee210d077443dde904
Guilhem de Cambiaire <guilhem.decambiaire@epfedu.fr>
2022-10-21 22:43:35 +0200

    récupération du TP1 faite avec TP0 aussi

---
+++ TP2_convertisseurObjet_DECAMBIAIRE/src/tp2_convertisseurobjet_decambiaire/TP2_convertisseurObjet_DECAMBIAIRE.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseurobjet_decambiaire;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author guilh
+ */
+public class TP2_convertisseurObjet_DECAMBIAIRE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        double temp;
+        double ind;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, saisissez une valeur :");
+        temp = sc.nextDouble();
+        System.out.println("Saisissez la conversion que vous souhaiter effectuer :\n1) De Celcius vers Kelvin\n2) De Kelvin vers Celcius\n...\n6) De Farenheit vers Kelvin");
+        ind = sc.nextDouble();
+        
+        //Convertisseur obj1 = new Convertisseur();
+        //Convertisseur obj2 = new Convertisseur();
+
+    }
+    
+}
