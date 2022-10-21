f84fb7a573ac38087fe71968b7dcdb1df4aaf001
Guilhem de Cambiaire <guilhem.decambiaire@epfedu.fr>
2022-10-21 17:05:31 +0200

    .14 création de plusieurs bouteilles

---
+++ TP2_Bieres_DECAMBIAIRE/src/tp2_bieres_decambiaire/BouteilleBiere.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_decambiaire;
+
+/**
+ *
+ * @author guilh
+ */
+public class BouteilleBiere {
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;   
+    
+    public void lireEtiquette() {
+    System.out.println("Bouteille de " + nom +" (" + degreAlcool +" degres) \nBrasserie : " + brasserie ) ;   
+    }
+    
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+    nom = unNom;
+    degreAlcool = unDegre;
+    brasserie = uneBrasserie;
+    ouverte = false;
+    }
+    
+    public boolean Décapsuler() {
+    if (ouverte==false){
+        ouverte=true;
+        System.out.println("Biere ouverte");
+        return true;
+    }else{
+        System.out.println("Erreur : biere déjà ouverte");
+        return false;
+        }           
+    }
+    
+    public String toString() {
+    String chaine_a_retourner;
+    chaine_a_retourner = nom + " " +brasserie +" (" + degreAlcool + " degrés) Ouverte ? ";
+    if (ouverte == true ) chaine_a_retourner += "oui" ;
+    else chaine_a_retourner += "non" ;
+    return chaine_a_retourner ;
+}
+    
+}
+
+
+
+    
+    
+
+
