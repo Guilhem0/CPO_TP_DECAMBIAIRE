85d2b8f55c45bb2d70bd5fc98cb3341ad511f91a
Guilhem de Cambiaire <guilhem.decambiaire@epfedu.fr>
2022-10-24 22:51:24 +0200

    2.3 je me bast avec le nombre de conversion j'ai tjr pas reussi

--- TP2_convertisseurObjet_DECAMBIAIRE/src/tp2_convertisseurobjet_decambiaire/Convertisseur.java
+++ TP2_convertisseurObjet_DECAMBIAIRE/src/tp2_convertisseurobjet_decambiaire/Convertisseur.java
@@ -15,28 +15,34 @@ public class Convertisseur {
     }
     
     public static double CelciusVersKelvin (double tCelcius) {
-        tCelcius=tCelcius+275.13;
+        tCelcius=tCelcius+273.15;
+        //Convertisseur.nbConversions=nbConversions+1;
         return tCelcius;
     }
     public static double KelvinVersCelcius(double tKelvin) {
-        tKelvin=tKelvin-275.13;
+        tKelvin=tKelvin-273.15;
         return tKelvin;
     }
     public static double FarenheitVersCelcius(double tFarenheit) {
         tFarenheit=(tFarenheit-32)/1.8;
         return tFarenheit;
     }
-    public static double CelciusVersFarenheit(double tCelsius) {
-        tCelsius=(tCelsius*1.8)+32;
-        return tCelsius;
+    public static double CelciusVersFarenheit(double tCelcius) {
+        tCelcius=(tCelcius*1.8)+32;
+        return tCelcius;
     }
     public static double KelvinVersFarenheit(double tKelvin) {
-        tKelvin=(tKelvin-32)/1.8+275.13;
+        tKelvin=(tKelvin-273.15)*(9/5)+32;
         return tKelvin;
     }
     public static double FarenheitVersKelvin(double tFarenheit) {
-        tFarenheit=(tFarenheit-275.13)*1.8+32;
+        tFarenheit=(tFarenheit-32)*(5/9)+273.15;
         return tFarenheit;
+    }
+    
+    @Override
+    public String toString () {
+    return "nb de conversions "+ nbConversions;
     }
 
 }
